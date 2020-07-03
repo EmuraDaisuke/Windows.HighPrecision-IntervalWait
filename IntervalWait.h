ef44627e7741a771dc117c2b7c0e05e0e2c931b8
dia_maisun <dia_maisun@jcom.home.ne.jp>
2020-07-03 13:43:26 +0900

    .

---
+++ IntervalWait.h
@@ -0,0 +1,129 @@
+#pragma once
+
+
+
+#include <cstddef>
+
+#include <windows.h>
+#include <avrt.h>
+
+#pragma comment(lib, "avrt.lib")
+
+
+
+class IntervalWait {
+    public:
+        ~IntervalWait()
+        {
+            if (mhShutdown) SetEvent(mhShutdown);
+            if (mhThread) WaitForSingleObject(mhThread, INFINITE);
+            
+            CloseHandle(mhThread);
+            CloseHandle(mhShutdown);
+            CloseHandle(mhWait);
+        }
+        
+        
+        
+        IntervalWait(int64_t nsec)
+        :mInterval(nsec)
+        ,mhWait(NULL)
+        ,mhShutdown(NULL)
+        ,mhThread(NULL)
+        {
+            mhWait = CreateEvent(NULL, FALSE, TRUE, NULL);
+            mhShutdown = CreateEvent(NULL, FALSE, FALSE, NULL);
+            mhThread = CreateThread(NULL, 0, Thread, this, 0, NULL);
+        }
+        
+        
+        
+        void Wait()
+        {
+            WaitForSingleObject(mhWait, INFINITE);
+        }
+    
+    
+    
+    private:
+        DWORD Thread()
+        {
+            HMODULE hDll = NULL;
+            HANDLE hRevert = NULL;
+            HANDLE hTimer = NULL;
+            
+            using ProcNtQueryTimerResolution = LONG(_stdcall*)(PULONG maximum, PULONG minimum, PULONG current);
+            using ProcNtSetTimerResolution = LONG(_stdcall*)(ULONG resolution, BOOL set, PULONG current);
+            ProcNtQueryTimerResolution NtQueryTimerResolution = nullptr;
+            ProcNtSetTimerResolution NtSetTimerResolution = nullptr;
+            
+            ULONG Maximum = 0;
+            ULONG Minimum = 0;
+            ULONG Current = 0;
+            
+            do {
+                hDll = LoadLibrary("ntdll.dll");
+                if (!hDll) break;
+                
+                NtQueryTimerResolution = (ProcNtQueryTimerResolution) GetProcAddress(hDll, "NtQueryTimerResolution");
+                NtSetTimerResolution = (ProcNtSetTimerResolution) GetProcAddress(hDll, "NtSetTimerResolution");
+                if (!NtQueryTimerResolution) break;
+                if (!NtSetTimerResolution) break;
+                
+                NtQueryTimerResolution(&Maximum, &Minimum, &Current);
+                NtSetTimerResolution(Minimum, TRUE, &Current);
+                
+                DWORD TaskIndex = 0;
+                hRevert = AvSetMmThreadCharacteristics("PRO Audio", &TaskIndex);
+                hTimer = CreateWaitableTimer(NULL, TRUE, NULL);
+                if (!hRevert) break;
+                if (!hTimer) break;
+                
+                LARGE_INTEGER Frequency;
+                LARGE_INTEGER Counter;
+                QueryPerformanceFrequency(&Frequency);
+                QueryPerformanceCounter(&Counter);
+                
+                int64_t Precision = (/*sec*/1ll * /*msec*/1000 * /*usec*/1000 * /*nsec*/1000) / Frequency.QuadPart;
+                int64_t Absolute = Counter.QuadPart * Precision;
+                int64_t Relative = 0;
+                
+                while (WaitForSingleObject(mhShutdown, 0) != WAIT_OBJECT_0){
+                    Counter.QuadPart = (mInterval + Relative) / -Precision;
+                    SetWaitableTimer(hTimer, &Counter, 0, NULL, NULL, FALSE);
+                    WaitForSingleObject(hTimer, INFINITE);
+                    
+                    SetEvent(mhWait);
+                    
+                    QueryPerformanceCounter(&Counter);
+                    Absolute += mInterval;
+                    Relative = Absolute - (Counter.QuadPart * Precision);
+                }
+            } while (false);
+            
+            if (hTimer) CloseHandle(hTimer);
+            if (hRevert) AvRevertMmThreadCharacteristics(hRevert);
+            
+            if (NtSetTimerResolution) NtSetTimerResolution(0, FALSE, &Current);
+            
+            if (hDll) FreeLibrary(hDll);
+            return 0;
+        }
+        
+        
+        
+        static DWORD Thread(void* pVoid)
+        {
+            auto pThis = reinterpret_cast<IntervalWait*>(pVoid);
+            return pThis->Thread();
+        }
+    
+    
+    
+    private:
+        int64_t mInterval;
+        
+        HANDLE mhWait;
+        HANDLE mhShutdown;
+        HANDLE mhThread;
+};
---
+++ Main.cpp
@@ -0,0 +1,49 @@
+
+
+
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+
+#include <windows.h>
+#include <winuser.h>
+#include <conio.h>
+
+#include "./IntervalWait.h"
+
+#pragma comment(lib, "user32.lib")
+
+
+
+double Now()
+{
+    using namespace std::chrono;
+    return static_cast<double>(duration_cast<nanoseconds>(steady_clock::now().time_since_epoch()).count()) / 1000000000;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+//  IntervalWait Interval(/*ms*/1 * /*us*/1000 * /*ns*/1000);
+//  IntervalWait Interval(/*us*/1000 * /*ns*/1000);
+    IntervalWait Interval(3333333);
+    
+    double vOld = Now();
+    double vNow = Now();
+    double vSum = 0;
+    int nSum = 0;
+    
+    while (!(GetAsyncKeyState(VK_ESCAPE) < 0)){
+        Interval.Wait();
+        
+        vOld = vNow;
+        vNow = Now();
+        vSum += vNow - vOld;
+        ++nSum;
+        
+        std::cout << std::fixed << std::setprecision(9) << (vNow - vOld) << " " << (vSum / nSum) << std::endl;
+    }
+    
+    return 0;
+}
